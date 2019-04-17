f03fc659e05909166d001623ad2180dea0a0c9dd
Leszek Andrukanis <andrukanisleszek@gmail.com>
2019-04-17 16:54:27 +0200

    Change signature.

--- dependencies/recipes/default.rb
+++ dependencies/recipes/default.rb
@@ -6,6 +6,6 @@
 
 execute "yum -y install curl"
 execute "yum -y install htop"
-execute "curl -sSL https://get.rvm.io | bash -s stable --ruby"
 execute "sudo gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
+execute "curl -sSL https://get.rvm.io | bash -s stable --ruby"
 execute "rvm install 2.5.3"
